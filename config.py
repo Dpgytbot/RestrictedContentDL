c4abed953680b8b21325bbb3df21959d2f5bfe80
Dpgytbot <155309888+Dpgytbot@users.noreply.github.com>
2024-12-22 13:51:13 +0530

    config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-# Copyright (C) @TheSmartBisnu
-# Channel: https://t.me/itsSmartDev
+# Copyright (C) @AndroidV14
+# Channel: https://t.me/blackhatindia47
 
 # Pyrogram setup
-API_ID = "123456"  # Replace this API ID with your actual API ID
-API_HASH = "123xxxxxxx"  # Replace this API HASH with your actual API HASH
-BOT_TOKEN = "12345678:xxxxxxxxxxxxxxxxx"  # Replace this BOT_TOKEN
+API_ID = "22416655"  # Replace this API ID with your actual API ID
+API_HASH = "e53eecba602443041994af39d029f3f6"  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "7039723736:AAH_Lml27ej9uFT2c00glQM0um1t3tIFCwc"  # Replace this BOT_TOKEN
 SESSION_STRING = "xxxxxxxxxxxxxxxxxxxxxxx" # Replace this SESSION STRING with your actual SESSION_STRING
